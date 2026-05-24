0624bfee97ac0884e09aae7c6e498b17e3664745
Nandana C K <nandanack@gmail.com>
2026-05-24 22:05:49 +0530

    setup react typescript and tailwind

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-green-100 flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-green-700">SpendSmart 💰</h1>
+    </div>
+  )
+}
+
+export default App
